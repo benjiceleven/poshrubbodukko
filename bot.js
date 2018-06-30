86e9bfc7c6aee74ee577a5f3c5cd994aad01dbcb
benjiceleven <40725301+benjiceleven@users.noreply.github.com>
2018-06-30 18:47:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,20 +6,44 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === '.ping') {
     	message.reply('pong');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'Are you?') {
+    if (message.content === '.Are you?') {
     	message.reply('I am.');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'Why?') {
+    if (message.content === '.Why?') {
     	message.reply('Who knows?');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '.commands') {
+    	message.reply('Commands: .Are you? .ping .Why? .yt .twitch .creator');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '.yt') {
+    	message.reply('My Youtube: https://www.youtube.com/channel/UC7fHF0SUMGd9x32odZ9hv_A?view_as=subscriber');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '.twitch?') {
+    	message.reply('My Twitch: https://www.twitch.tv/C_011');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '.creator') {
+    	message.reply('Benji Celeven, the most bestest of frens!');
   	}
 });
 
